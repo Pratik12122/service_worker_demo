2467aa454a5da3ea2dc80f01d4e7f1ffa741bdd5
Pratik Nandoskar <Pratik.nandoskar2@gmail.com>
2018-11-25 17:54:11 +0530

    cache offline.html file using SW

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-const VERSION = 'v2';
+const VERSION = 'v4 ';
 
 function log(message) {
   console.log(VERSION, message);
@@ -6,9 +6,20 @@ function log(message) {
 
 log('Service Worker Added');
 
-self.addEventListener('install', () => {
-  log(' Version is installed');
-});
+self.addEventListener('install', event => event.waitUntil(installServiceWorker()));
+
+async function installServiceWorker(){
+  log('SW installation started');
+  const request = new Request('offline.html');
+  const response = await fetch(request);
+  if(response.status != 200 ) {
+      throw new Error('could not load offline page.');
+  }
+
+  const cache = await caches.open('app-cache');
+  cache.put(request, response);
+  log('cached offline.html successfully.');
+}
 
 self.addEventListener('activate', () => {
   log(' Version is activated');
