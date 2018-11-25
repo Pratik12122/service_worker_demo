247656c526e2ce00f1daaa74ed20a14d8c24c935
Pratik Nandoskar <Pratik.nandoskar2@gmail.com>
2018-11-25 16:37:33 +0530

    SW lifecycle events

--- src/sw.js
+++ src/sw.js
@@ -1,8 +1,15 @@
-const VERSION = 'v1';
+const VERSION = 'v2';
 
 function log(message) {
   console.log(VERSION, message);
 }
 
-
 log('Service Worker Added');
+
+self.addEventListener('install', () => {
+  log(' Version is installed');
+});
+
+self.addEventListener('activate', () => {
+  log(' Version is activated');
+});
